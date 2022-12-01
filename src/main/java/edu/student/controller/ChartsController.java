036c074f9e6381f8c4cb594d29ed1ada9d4545ce
celsiusdv <celsius.dev@protonmail.com>
2022-11-30 22:51:45 -0300

    line chart modified

--- src/main/java/edu/student/controller/ChartsController.java
+++ src/main/java/edu/student/controller/ChartsController.java
@@ -1,9 +1,6 @@
 package edu.student.controller;
 
 import edu.student.services.AttendanceService;
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -29,29 +26,14 @@ public class ChartsController implements Initializable, EventHandler<ActionEvent
     @FXML Button searchBtn;
     @FXML TextField idField;
 
-    @FXML LineChart<String,Number> mathChart;//container of months and values
-    @FXML CategoryAxis mathCategoryAxis;//horizontal axis for months names
-    @FXML NumberAxis mathAttendanceAxis;//vertical axis indicator for attendances values
+    @FXML LineChart<String,Number> subjectsChart;//container of months and values
+    @FXML CategoryAxis subjectCategoryAxis;//horizontal axis for months names
+    @FXML NumberAxis subjectAttendanceAxis;//vertical axis indicator for attendances values
+
     XYChart.Series<String, Number> mathSeries;//collection of values(attendances, months) to set in the chart
-
-    @FXML LineChart<String,Number> englishChart;
-    @FXML CategoryAxis engCategoryAxis;
-    @FXML NumberAxis engAttendanceAxis;
     XYChart.Series<String, Number> engSeries;
-
-    @FXML LineChart<String,Number> programmingChart;
-    @FXML CategoryAxis progCategoryAxis;
-    @FXML NumberAxis progAttendanceAxis;
     XYChart.Series<String, Number> progSeries;
-
-    @FXML LineChart<String,Number> physicsChart;
-    @FXML CategoryAxis physicsCategoryAxis;
-    @FXML NumberAxis physicsAttendanceAxis;
     XYChart.Series<String, Number> physicsSeries;
-
-    @FXML LineChart<String,Number> economicsChart;
-    @FXML CategoryAxis economicsCategoryAxis;
-    @FXML NumberAxis economicsAttendanceAxis;
     XYChart.Series<String, Number> economicsSeries;
 
     String[] months;
@@ -62,149 +44,55 @@ public class ChartsController implements Initializable, EventHandler<ActionEvent
     public void initialize(URL location, ResourceBundle resources) {
         months= new String[]{"mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov"};
         attendanceService=new AttendanceService();
-        setMathChartDescription();
-        setMathChart();
-
-        setEnglishChartDescription();
-        setEnglishChart();
-
-        setProgrammingChartDescription();
-        setProgrammingChart();
-
-        setPhysicsChartDescription();
-        setPhysicsChart();
-
-        setEconomicsChartDescription();
-        setEconomicsChart();
+        setChartVerticalRange();
+        setSubjectChart();
     }
 
-    public void setMathChart(){
+    public void setSubjectChart(){
         mathSeries = new XYChart.Series();
         mathSeries.setName("Math");
-        for(int i=0; i<months.length; i++){
-            //adding values to the chart
-            mathSeries.getData().add(new XYChart.Data(months[i], 0));
-        }
-        mathChart.getData().add(mathSeries);
-    }
-    public void setEnglishChart(){
-        engSeries=new XYChart.Series<>();
+        engSeries = new XYChart.Series();
         engSeries.setName("English");
-        for(int i=0; i<months.length; i++){
-            //adding values to the chart
-            engSeries.getData().add(new XYChart.Data(months[i], 0));
-        }
-        englishChart.getData().add(engSeries);
-    }
-    public void setProgrammingChart(){
-        progSeries=new XYChart.Series<>();
+        progSeries = new XYChart.Series();
         progSeries.setName("Programming");
-        for(int i=0; i<months.length; i++){
-            //adding values to the chart
-            progSeries.getData().add(new XYChart.Data(months[i], 0));
-        }
-        programmingChart.getData().add(progSeries);
-    }
-    public void setPhysicsChart(){
-        physicsSeries=new XYChart.Series<>();
+        physicsSeries = new XYChart.Series();
         physicsSeries.setName("Physics");
-        for(int i=0; i<months.length; i++){
-            //adding values to the chart
-            physicsSeries.getData().add(new XYChart.Data(months[i], 0));
-        }
-        physicsChart.getData().add(physicsSeries);
-    }
-    public void setEconomicsChart(){
-        economicsSeries=new XYChart.Series<>();
+        economicsSeries = new XYChart.Series();
         economicsSeries.setName("Economics");
         for(int i=0; i<months.length; i++){
-            //adding values to the chart
-            economicsSeries.getData().add(new XYChart.Data(months[i], 0));
+            //adding default values to the chart
+            mathSeries.getData().add(new XYChart.Data(months[i], 0));
+            engSeries.getData().add(new XYChart.Data(months[i], 1));
+            progSeries.getData().add(new XYChart.Data(months[i], 2));
+            economicsSeries.getData().add(new XYChart.Data(months[i], 3));
+            physicsSeries.getData().add(new XYChart.Data(months[i], 4));
         }
-        economicsChart.getData().add(economicsSeries);
+        subjectsChart.getData().addAll(mathSeries,engSeries,progSeries,physicsSeries,economicsSeries);
     }
 
-
-
-    public void setMathChartValues(int id){
-        //update values for math chart
-        int[] attendancesPerMonth=attendanceService.showMathAttendances(id);
+    public void setChartValues(int id){
+        //update values for every subject in chart
+        int[] mathAttendances=attendanceService.showMathAttendances(id);
+        int[] engAttendances=attendanceService.showEnglishAttendances(id);
+        int[] progAttendances=attendanceService.showProgrammingAttendances(id);
+        int[] physAttendances=attendanceService.showPhysicsAttendances(id);
+        int[] econAttendances=attendanceService.showEconomicsAttendances(id);
         for (int i=0;i<months.length; i++){
-            mathSeries.getData().get(i).setYValue(attendancesPerMonth[i]);
+            mathSeries.getData().get(i).setYValue(mathAttendances[i]);
+            engSeries.getData().get(i).setYValue(engAttendances[i]);
+            progSeries.getData().get(i).setYValue(progAttendances[i]);
+            physicsSeries.getData().get(i).setYValue(physAttendances[i]);
+            economicsSeries.getData().get(i).setYValue(econAttendances[i]);
         }
     }
-    public void setEnglishChartValues(int id){
-        //update values for english chart
-        int[] attendancesPerMonth=attendanceService.showEnglishAttendances(id);
-        for (int i=0;i<months.length; i++){
-            engSeries.getData().get(i).setYValue(attendancesPerMonth[i]);
-        }
-    }
-    public void setProgrammingChartValues(int id){
-        //update values for programming chart
-        int[] attendancesPerMonth=attendanceService.showProgrammingAttendances(id);
-        for (int i=0;i<months.length; i++){
-            progSeries.getData().get(i).setYValue(attendancesPerMonth[i]);
-        }
-    }
-    public void setPhysicsChartValues(int id){
-        //update values for physics chart
-        int[] attendancesPerMonth=attendanceService.showPhysicsAttendances(id);
-        for (int i=0;i<months.length; i++){
-            physicsSeries.getData().get(i).setYValue(attendancesPerMonth[i]);
-        }
-    }
-    public void setEconomicsChartValues(int id){
-        //update values for economics chart
-        int[] attendancesPerMonth=attendanceService.showEconomicsAttendances(id);
-        for (int i=0;i<months.length; i++){
-            economicsSeries.getData().get(i).setYValue(attendancesPerMonth[i]);
-        }
-    }
-
-
-    public void setMathChartDescription(){
+    public void setChartVerticalRange(){
         //setting vertical indicator for the line
-        mathAttendanceAxis.setAutoRanging(false);
-        mathAttendanceAxis.setLowerBound(0);
-        mathAttendanceAxis.setUpperBound(30);
-        mathAttendanceAxis.setTickUnit(5);
-        mathAttendanceAxis.setLabel("attendances");
+        subjectAttendanceAxis.setAutoRanging(false);
+        subjectAttendanceAxis.setLowerBound(0);
+        subjectAttendanceAxis.setUpperBound(30);
+        subjectAttendanceAxis.setTickUnit(5);
+        subjectAttendanceAxis.setLabel("attendances");
     }
-    public void setEnglishChartDescription(){
-        //setting vertical indicator for the line
-        engAttendanceAxis.setAutoRanging(false);
-        engAttendanceAxis.setLowerBound(0);
-        engAttendanceAxis.setUpperBound(30);
-        engAttendanceAxis.setTickUnit(5);
-        engAttendanceAxis.setLabel("attendances");
-    }
-    public void setProgrammingChartDescription(){
-        //setting vertical indicator for the line
-        progAttendanceAxis.setAutoRanging(false);
-        progAttendanceAxis.setLowerBound(0);
-        progAttendanceAxis.setUpperBound(30);
-        progAttendanceAxis.setTickUnit(5);
-        progAttendanceAxis.setLabel("attendances");
-    }
-    public void setPhysicsChartDescription(){
-        //setting vertical indicator for the line
-        physicsAttendanceAxis.setAutoRanging(false);
-        physicsAttendanceAxis.setLowerBound(0);
-        physicsAttendanceAxis.setUpperBound(30);
-        physicsAttendanceAxis.setTickUnit(5);
-        physicsAttendanceAxis.setLabel("attendances");
-    }
-    public void setEconomicsChartDescription(){
-        //setting vertical indicator for the line
-        economicsAttendanceAxis.setAutoRanging(false);
-        economicsAttendanceAxis.setLowerBound(0);
-        economicsAttendanceAxis.setUpperBound(30);
-        economicsAttendanceAxis.setTickUnit(5);
-        economicsAttendanceAxis.setLabel("attendances");
-    }
-
-
 
     @Override
     public void handle(ActionEvent event) {
@@ -213,11 +101,7 @@ public class ChartsController implements Initializable, EventHandler<ActionEvent
         }
         if(searchBtn.equals(event.getSource())){
             int id= Integer.parseInt(idField.getText());
-            setMathChartValues(id);
-            setEnglishChartValues(id);
-            setProgrammingChartValues(id);
-            setPhysicsChartValues(id);
-            setEconomicsChartValues(id);
+            setChartValues(id);
         }
     }
 
