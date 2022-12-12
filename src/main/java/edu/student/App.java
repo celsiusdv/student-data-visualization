e2e4f2612cdd753b1dce791d3854ec8fdc1689ad
celsiusdv <celsius.dev@protonmail.com>
2022-12-11 23:09:58 -0300

    project packaged to jar

--- src/main/java/edu/student/App.java
+++ src/main/java/edu/student/App.java
@@ -4,8 +4,6 @@ import java.time.LocalDate;
 
 public class App {
     public static void main( String[] args ) {
-        //bring application.main here
-
-        System.out.println(LocalDate.of(2012,11,22));
+        Main.main(args);
     }
 }
